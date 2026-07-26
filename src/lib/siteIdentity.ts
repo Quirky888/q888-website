01891c86c803a55e911a2d8a3bba6e671c86db2b
Quirky888 <alexander.sestakovs@gmail.com>
2026-07-26 19:44:31 +0100

    feat: add image licensing metadata to SEO schema and update copyright page with formal policy details

--- src/lib/siteIdentity.ts
+++ src/lib/siteIdentity.ts
@@ -6,6 +6,8 @@ export const DEFAULT_DESCRIPTION =
   "Q888 is the Edinburgh-based artist-developer practice of Aleksandrs Sestakovs, building speculative art, AI research, symbolic systems, and functioning interfaces.";
 
 export const DEFAULT_SOCIAL_IMAGE = "/images/Q888_hero.webp";
+export const IMAGE_LICENSE_URL = `${SITE_URL}/copyright/`;
+export const IMAGE_LICENSE_ACQUISITION_URL = `${SITE_URL}/copyright/#image-licensing`;
 
 export const ARTIST_PROFILES = [
   "https://instagram.com/quirky_888",
@@ -18,4 +20,3 @@ export const ARTIST_ID = `${SITE_URL}/#aleksandrs-sestakovs`;
 export const WEBSITE_ID = `${SITE_URL}/#website`;
 
 export type SchemaNode = Record<string, unknown>;
-
